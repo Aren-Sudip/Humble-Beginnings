57e312e3115ef24825aa110bf1417f9a8849a577
Sudip Chaulagain <sudip-logs@Sudips-MacBook-Air.local>
2026-06-19 06:37:31 +0900

    feat:new things in  swiper

--- simpleJS/practice.js
+++ simpleJS/practice.js
@@ -1,12 +1,19 @@
-
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
   loop: true,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
-
+  autoplay: {
+    delay: 1000, // Time in ms before switching (3 seconds)
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // Allows users to click dots to jump to a slide
+  },
+  effect: "fade", // or 'cube', 'coverflow', 'flip', 'creative'
+  fadeEffect: { crossFade: true },
 });
